bfa2ab709cda689c9f4208230f1b20546c554f5e
Daniel Wehner <dawehner@googlemail.com>
2011-02-02 17:52:43 +0100

    initial real test version of arrays

--- arrays.c
+++ arrays.c
@@ -2,5 +2,22 @@
 #include <math.h>
 
 int main() {
+  int Ary[5] = { 1, 1, 1, 1, 1 };
   return 0;
 }
+
+int array_middle(int *Arr) {
+  int middle, sum, size;
+
+  size = sizeof(Arr);
+  middle = 0;
+  sum = 0;
+
+  for( i = 0; i < sizeof(Arr); ++i ) {
+    middle += Arr[i];
+  }
+  middle = sum / size;
+
+  return size;
+}
+  
