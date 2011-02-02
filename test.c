957ea357d959b04d57614bb60e0ce19a6c4f5d3c
Daniel Wehner <dawehner@googlemail.com>
2011-02-02 17:29:16 +0100

    add some mf solutions

--- test.c
+++ test.c
@@ -1,17 +1,40 @@
 ﻿/* Das Hello-World-Programm */
 
 #include <stdio.h>
+#include <math.h>
 
 int main() {
-  int input;
+  int input_a, input_b, input_c;
   // Print out something simple
-  printf("Hello World!\n");
+  // printf("Hello World!\n");
 
   // Print out some input
-  scanf("%d", &input);
+  // scanf("%d", &input);
   
-  printf("%d", input);
-    
+  // printf("%d", input);
+  
+  // Get some values.
+  printf("Please input a x^2 + b x + c = 0");
+  scanf("%lf", &input_a);
+  scanf("%lf", &input_b);
+  scanf("%lf", &input_c);
+  
+  printf("%ls", mf_solution(input_a, input_b, input_c);
   return 0;
 }
 
+double mf_solution(double a, double b, double c, int positive = TRUE) {
+  double determinante_sqrt, return_value;
+
+  determinante_sqrt = sqrt(mf_determinante(a, b, c));
+
+  return_value = (- b * determinante_sqrt ) / (2 * a);
+  if (!positive) {
+    return_value *= -1;
+  }
+  return return_value;
+}	
+
+double mf_determinante(double a, double b, double c) {
+  return b * b - 4 * a * c;
+}
