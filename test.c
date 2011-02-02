70a86e39f252bdfbe1f0535a1526db3f3b7e5538
Daniel Wehner <dawehner@googlemail.com>
2011-02-02 17:14:00 +0100

    use initial hello world

--- test.c
+++ test.c
@@ -1,20 +1,10 @@
-﻿
+﻿/* Das Hello-World-Programm */
+
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
-        int *ptr;
-
-        ptr = malloc(sizeof *ptr);
-
-        if(!ptr) {
-                printf("Speicher kann nicht bereitgestellt werden\n");
-        } else {
-                printf("Speicher bereitgestellt\n");
-                *ptr = 70;
-                free(ptr);
-        }
-  
-        return EXIT_SUCCESS;
+  printf("Hello World!\n");
+    
+  return 0;
 }
