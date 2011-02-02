17714d1b28b8c6e0a35cf840472f06a002e4e89b
Daniel Wehner <dawehner@googlemail.com>
2011-02-02 17:05:31 +0100

    test version

---
+++ test.c
@@ -0,0 +1,20 @@
+﻿
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+        int *ptr;
+
+        ptr = malloc(sizeof *ptr);
+
+        if(!ptr) {
+                printf("Speicher kann nicht bereitgestellt werden\n");
+        } else {
+                printf("Speicher bereitgestellt\n");
+                *ptr = 70;
+                free(ptr);
+        }
+  
+        return EXIT_SUCCESS;
+}
