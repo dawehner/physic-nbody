7f144f1cbc4ec8ae28ff11cb2d1231dc3f5f9d38
Daniel Wehner <daniel.wehner@erdfisch.de>
2011-02-05 23:50:48 +0100

    initial version of nbody.cpp

---
+++ nbody.cpp
@@ -0,0 +1,9 @@
+#include <Eigen/Core>
+
+// import most common Eigen types 
+USING_PART_OF_NAMESPACE_EIGEN
+
+int main(int, char *[])
+{
+  std::cout << "Hallo this is just a easy output" << std::endl();
+}
