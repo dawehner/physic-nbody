325e8dcb8ddf83d964717f9f0ec1c942fced4027
Daniel Wehner <daniel.wehner@erdfisch.de>
2011-02-05 23:56:30 +0100

    test

--- nbody.cpp
+++ nbody.cpp
@@ -5,5 +5,5 @@ USING_PART_OF_NAMESPACE_EIGEN
 
 int main(int, char *[])
 {
-  std::cout << "Hallo this is just a easy output" << std::endl();
+  std::cout << "Hallo this is just a easy output" << std::endl;
 }
